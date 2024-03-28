77c644f89752c065189c7c2830a76d5990e9a0b6
Maimooba Masood <maimoonamasoo18@gmail.com>
2024-03-28 06:42:13 +0500

    Created Number guessing game by Maimoona

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// 1) computer will generate a random number 
+// 2) user input for guessing number
+// 3) compare user input with computer generated number and show result
+const randomNumber = Math.floor(Math.random() * 10 + 2);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 12",
+    },
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("Congragulations! you are a winner");
+}
+else {
+    console.log("You guess wrong number");
+}
